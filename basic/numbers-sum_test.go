c6cc334e16d73f1299e9a6fa7ec68c68859cc209
erni93 <erni.delvalle@outlook.com>
2020-05-30 00:30:54 +0200

    add random slice generator

--- basic/numbers-sum_test.go
+++ basic/numbers-sum_test.go
@@ -1,26 +1,44 @@
 package basic
 
-import "testing"
+import (
+	"math/rand"
+	"sort"
+	"testing"
+	"time"
+)
+
+func generateRandomSlice(num int, limit int) []int {
+	numbers := make([]int, 0, num)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < num; i++ {
+		numbers = append(numbers, rand.Intn(limit))
+	}
+	return numbers
+}
 
 func BenchmarkTwoNumbersSumFor(b *testing.B) {
-	numbers := []int{5, -2, 10, 8, 6}
+	numbers := generateRandomSlice(12345, 15)
 	sum := 15
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		TwoNumbersSumFor(numbers, sum)
 	}
 }
 
 func BenchmarkTwoNumbersSumMap(b *testing.B) {
-	numbers := []int{5, -2, 10, 8, 6}
+	numbers := generateRandomSlice(12345, 15)
 	sum := 15
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		TwoNumbersSumMap(numbers, sum)
 	}
 }
 
 func BenchmarkTwoNumbersSumShrinking(b *testing.B) {
-	numbers := []int{-2, 2, 3, 7, 9}
-	sum := 10
+	numbers := generateRandomSlice(12345, 20)
+	sort.Ints(numbers)
+	sum := 20
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		TwoNumbersSumShrinking(numbers, sum)
 	}
