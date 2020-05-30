da4f820ed2890160c94fa671673997803a12d571
erni93 <erni.delvalle@outlook.com>
2020-05-30 14:38:47 +0200

    fix numbers sum demo

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fmt.Printf("[TwoNumbersSumFor] Input: %v -> %v\n", []int{5, -2, 10, 8, 6}, basic.TwoNumbersSumFor([]int{5, -2, 10, 8, 6}, 15))
-	fmt.Printf("[TwoNumbersSumMap] Input: %v -> %v\n", []int{5, -2, 10, 8, 6}, basic.TwoNumbersSumFor([]int{5, -2, 10, 8, 6}, 15))
-	fmt.Printf("[TwoNumbersSumMap] Input: %v -> %v\n", []int{-2, 2, 3, 7, 9}, basic.TwoNumbersSumShrinking([]int{-2, 2, 3, 7, 9}, 10))
+	fmt.Printf("[TwoNumbersSumFor] Input: %v want %d -> %v\n", []int{5, -2, 10, 8, 6}, 15, basic.TwoNumbersSumFor([]int{5, -2, 10, 8, 6}, 15))
+	fmt.Printf("[TwoNumbersSumMap] Input: %v want %d -> %v\n", []int{5, -2, 10, 8, 6}, 15, basic.TwoNumbersSumFor([]int{5, -2, 10, 8, 6}, 15))
+	fmt.Printf("[TwoNumbersSumShrinking] Input: %v want %d -> %v\n", []int{-2, 2, 3, 7, 9}, 10, basic.TwoNumbersSumShrinking([]int{-2, 2, 3, 7, 9}, 10))
 }
