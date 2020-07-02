209fc89ffc1bf284b3a08d8690188a0c16171346
erni93 <erni.delvalle@outlook.com>
2020-07-02 18:03:47 +0200

    Add Fibonacci test cases

--- basic/fibonacci_test.go
+++ basic/fibonacci_test.go
@@ -7,6 +7,11 @@ import (
 
 var fibNumbers = [4]int{6, 9, 14, 20}
 
+type fibonacciTest struct {
+	num  int
+	want int
+}
+
 func BenchmarkFibonacciRecursive(b *testing.B) {
 	for _, num := range fibNumbers {
 		b.Run(fmt.Sprintf("n=%d", num), func(b *testing.B) {
@@ -32,6 +37,50 @@ func BenchmarkFibonacciSpace(b *testing.B) {
 		b.Run(fmt.Sprintf("n=%d", num), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				FibonacciDynamic(num)
+			}
+		})
+	}
+}
+
+func testModels() []fibonacciTest {
+	return []fibonacciTest{
+		{num: 6, want: 8},
+		{num: 7, want: 13},
+		{num: 8, want: 21},
+		{num: 9, want: 34},
+		{num: 10, want: 55},
+		{num: 11, want: 89},
+	}
+}
+
+func TestFibonacciRecursive(t *testing.T) {
+	tests := testModels()
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("n=%d", tt.num), func(t *testing.T) {
+			if got := FibonacciRecursive(tt.num); got != tt.want {
+				t.Errorf("FibonacciRecursive() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFibonacciFibonacciDynamic(t *testing.T) {
+	tests := testModels()
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("n=%d", tt.num), func(t *testing.T) {
+			if got := FibonacciDynamic(tt.num); got != tt.want {
+				t.Errorf("FibonacciDynamic() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFibonacciSpace(t *testing.T) {
+	tests := testModels()
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("n=%d", tt.num), func(t *testing.T) {
+			if got := FibonacciSpace(tt.num); got != tt.want {
+				t.Errorf("FibonacciSpace() = %v, want %v", got, tt.want)
 			}
 		})
 	}
